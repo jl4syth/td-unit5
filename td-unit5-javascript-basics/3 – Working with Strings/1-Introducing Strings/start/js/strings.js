77d6cd4a9fe6d351ac65f01d0d8fb9d89ab93e7a
jl.forsyth <lforsyth@berkeley.edu>
2021-05-09 11:18:34 -0700

    Strings, transforming strings, methods.

--- td-unit5-javascript-basics/3 – Working with Strings/1-Introducing Strings/start/js/strings.js
+++ td-unit5-javascript-basics/3 – Working with Strings/1-Introducing Strings/start/js/strings.js
@@ -1,1 +1,8 @@
-console.log("Hello, world!");
+const htmlSnippet = '<h1 class = "headline">My Headline</h1>';
+const message = 'I\'m a programmer!';
+
+const multiline = "Hello, student. \
+Welcome to JavaScript Basics. \
+I hope you learn a lot.";
+
+console.log(multiline);
