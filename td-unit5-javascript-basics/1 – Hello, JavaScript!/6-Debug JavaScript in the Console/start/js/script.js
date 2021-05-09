463cd37e4a1feb66bff398dac0d29fab156c9a7d
jl.forsyth <lforsyth@berkeley.edu>
2021-05-08 17:05:23 -0700

    js basics: console.log, alert, doc.write

--- td-unit5-javascript-basics/1 – Hello, JavaScript!/6-Debug JavaScript in the Console/start/js/script.js
+++ td-unit5-javascript-basics/1 – Hello, JavaScript!/6-Debug JavaScript in the Console/start/js/script.js
@@ -1,2 +1,5 @@
 
-alert("Hey, you're back for more?");
+alert("Hello, world!");
+console.log("Hello from the console.");
+alert("Thanks for visiting!");
+document.write("<h1>Welcome to my web page.</h1>");
